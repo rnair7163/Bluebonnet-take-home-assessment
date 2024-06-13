f90788c9f1d5c4da7e00d1c3734c1ba528c83952
Rahul Nair <rahulmnair1997@gmail.com>
2024-06-12 20:32:30 -0500

    code refactor

--- data_analysis.sql
+++ data_analysis.sql
@@ -8,6 +8,7 @@ Question 1 - How many people not registered as Democrats voted in the Primaries?
 select count(voter_id) as voter_count -- I am not distict count here since it's already mentioned that there are no duplicates
 from 2021_Special_Primaries
 where current_party <> 'D';
+-------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Question 2 - What percentage of Primary voters were born since 1990 (including 1990)?
@@ -16,6 +17,7 @@ select round((count(case when year_of_birth >= '1990' then voter_id end)::decima
        /
        count(voter_id)) * 100, 2) as voter_percentage -- rounding it up to 2 decimals
 from 2021_Special_Primaries;
+-------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Question 3 - How many voters voted in a Primary AND a General Election in 2021?
@@ -24,6 +26,7 @@ select count(sp.voter_id) as voter_count
 from 2021_Special_Primaries as sp 
 join 2021_Special_Generals as sg 
   on sp.voter_id = sg.voter_id;
+-------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Question 4 - How many voters voted in a Primary but not a General in 2021?
@@ -32,6 +35,7 @@ Question 4 - How many voters voted in a Primary but not a General in 2021?
 select voter_id 
 from 2021_Special_Primaries
 where voter_id not in (select voter_id from 2021_Special_Generals);
+-------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Question 5 - Produce counts for how many votes were cast in each Zip Code across both the Generals and Primaries 
